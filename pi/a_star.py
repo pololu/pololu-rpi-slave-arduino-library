433d1aef778d1328d1b59f5831de8923a5dbbb26
Kevin (Pololu) <dev-kevin@pololu.com>
2017-03-28 18:51:02 -0700

    a_star.py: remove unneeded (in python 3) bytearray call

--- pi/a_star.py
+++ pi/a_star.py
@@ -18,12 +18,12 @@ class AStar(object):
     # A delay of 0.0001 (100 us) after each write is enough to account
     # for the worst-case situation in our example code.
 
-    self.bus.write_byte(20,address)
+    self.bus.write_byte(20, address)
     time.sleep(0.0001)
     byte_list = []
     for n in range(0,size):
       byte_list.append(self.bus.read_byte(20))
-    return struct.unpack(format,bytes(bytearray(byte_list)))
+    return struct.unpack(format, bytes(byte_list))
 
   def write_pack(self, address, format, *data):
     data_array = list(struct.pack(format, *data))
